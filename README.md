8594a279a7452dcc1fbac4fe50730f766dc1f6ab
Tzavi727 <tzaviknows@gmail.com>
2026-02-08 17:16:39 -0300

    feat: robust file organizer using Java NIO, Streams and Lambdas

---
+++ README.md
@@ -0,0 +1,51 @@
+# File Organizer
+
+A program that organizes your files into folders according to their respective types.
+
+---
+
+## Features
+
+*   **Auto Find Path**
+    *   Auto searches for your downloads path.
+*   **Works with any Path**
+    *   You can manually type any path of your choice for the program to work.
+*   **Auto Create Folders**
+    *   The program auto create folders or check if they already exists so it doesn't make duplicates.
+*   **Extension Sorting**
+    *   It also auto sorts files by its extension (e.g., .png -> images folder).
+*   **Robust Code**
+    *   The program deals with input\directories validation ensuring no crashes.
+
+---
+
+## Technologies Used
+
+*   **Language:** Java
+*   **Development Environment:** Visual Studio Code
+*   **Key Java Concepts Applied:**
+    *   Control Flow (while loops)
+    *   Exception Handling (try-catch)
+    *   Console I/O (Scanner)
+    *   HashMap
+    *   Lambdas
+    *   java.nio.files (Library)
+
+---
+
+## How to Run
+
+1.  **Prerequisites:** You need to have the Java Development Kit (JDK) installed on your machine.
+2.  **Clone the repository:**
+    ```bash
+    git clone https://github.com/Tzavi727/File-Organizer.git
+    ```
+3.  **Navigate to the source folder and compile:**
+    ```bash
+    javac com/tzavi/fileorganizer/FileOrganizer.java
+    ```
+4.  **Run the application:**
+    ```bash
+    java com.tzavi.fileorganizer.FileOrganizer
+    ```
+*(Note: Running directly from an IDE like VS Code or IntelliJ is the recommended way.)*
---
+++ src/FileOrganizer.java
@@ -0,0 +1,117 @@
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class FileOrganizer {
+    public static Scanner scanner = new Scanner(System.in);
+    public static Map<String, String> rules = new HashMap<>();
+
+    public static void main(String[] args) throws Exception {
+        setRules();
+        Path userHome = null;
+        while (true) {
+            cleanscreen();
+            System.out.println("==================================================");
+            System.out.println("         DOWNLOADS FILE ORGANIZER v1.0           ");
+            System.out.println("   Automatically sort your files into folders     ");
+            System.out.println("==================================================");
+            System.out.println(
+                    "1 - Try Auto Find Donwloads Path\n2 - Manually Type Downloads Path\n3 - Manually Type Another Folder Path");
+            System.out.println("==================================================");
+            int userInput = getUserInput();
+            if (userInput == 1) {
+                userHome = Path.of(System.getProperty("user.home"));
+                userHome = userHome.resolve("Downloads");
+            } else if (userInput == 2 || userInput == 3) {
+                cleanscreen();
+                System.out.println("==================================================");
+                System.out.println("Manually Type Path Below: ");
+                System.out.println("==================================================");
+                String userHomeInput = scanner.nextLine();
+                userHome = Path.of(userHomeInput).toAbsolutePath();
+            }
+            if (userHome != null && Files.exists(userHome)) {
+                cleanscreen();
+                final Path finalPath = userHome;
+                try (Stream<Path> stream = Files.list(userHome)) {
+                    stream.forEach(file -> {
+                        if (Files.isRegularFile(file)) {
+                            String fileName = file.getFileName().toString();
+                            int dotIndex = fileName.lastIndexOf(".");
+                            if (dotIndex > 0) {
+                                String extension = fileName.substring(dotIndex + 1).toLowerCase();
+
+                                if (rules.containsKey(extension)) {
+                                    String destinationFolder = rules.get(extension);
+                                    moveFile(file, destinationFolder, finalPath);
+                                }
+                            }
+                        }
+                    });
+                } catch (Exception e) {
+                    cleanscreen();
+                    System.out.println("Something went wrong when trying to list files.");
+                }
+                System.out.println("Your downloads folder should now be organized!");
+                scanner.nextLine();
+                break;
+            } else {
+                cleanscreen();
+                System.out.printf("Path '%s' not found.\n", userHome);
+                System.out.println("Press ENTER to try again...");
+                scanner.nextLine();
+                userHome = null;
+            }
+        }
+    }
+
+    public static void setRules() {
+        rules.put("zip", "compressed");
+        rules.put("exe", "executables");
+        rules.put("jpg", "images");
+        rules.put("pdf", "documents");
+        rules.put("mp4", "videos");
+        rules.put("jpeg", "images");
+        rules.put("png", "images");
+
+    }
+
+    public static void moveFile(Path originalFile, String folderDestinationName, Path downloadsPath) {
+        Path target = downloadsPath.resolve(folderDestinationName);
+        try {
+            Files.createDirectories(target);
+            Path finalPath = target.resolve(originalFile.getFileName());
+            Files.move(originalFile, finalPath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void cleanscreen() {
+        System.out.println("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    public static int getUserInput() {
+        while (true) {
+            try {
+                String input = scanner.nextLine();
+                int choice = Integer.parseInt(input);
+                if (choice == 1 || choice == 2 || choice == 3) {
+                    return choice;
+                } else {
+                    System.out.println("Invalid option! Please type a NUMBER between 1 and 3:");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Error: Please type a NUMBER between 1 and 3:");
+            }
+        }
+    }
+}
