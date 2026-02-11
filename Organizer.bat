5c6b40d5b7efe6aae109d6ea1a45f6203b3255dc
Tzavi727 <tzaviknows@gmail.com>
2026-02-10 22:57:46 -0300

    refactor: clean up repository, apply gitignore and improve cleanscreen portability

---
+++ Organizer.bat
@@ -0,0 +1,3 @@
+@echo off
+java -jar FileOrganizer.jar
+pause
--- src/FileOrganizer.java
+++ src/FileOrganizer.java
@@ -1,4 +1,3 @@
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -95,8 +94,17 @@ public class FileOrganizer {
     }
 
     public static void cleanscreen() {
-        System.out.println("\033[H\033[2J");
-        System.out.flush();
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                System.out.println("\033[H\033[2J");
+                System.out.flush();
+            }
+        } catch (Exception e) {
+            for (int i = 0; i < 50; i++)
+                System.out.println();
+        }
     }
 
     public static int getUserInput() {
