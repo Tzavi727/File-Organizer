665b0176fe0e492f83a2b10dfc13d14cc5b7e8be
Tzavi727 <tzaviknows@gmail.com>
2026-03-20 21:32:27 -0300

    feat: implement new popup feature

---
+++ FileOrganizer/src/Views/DialogBox.axaml.cs
@@ -0,0 +1,40 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace FileOrganizer;
+
+public enum DialogType { Error, Confirm }
+public partial class DialogBox : Window
+{
+    public DialogBox(string title, string message, DialogType type)
+    {
+        InitializeComponent();
+        this.Title = title;
+        DialogMessage.Text = message;
+
+        switch (type)
+        {
+            case DialogType.Error:
+                ErrorIcon.IsVisible = true;
+                OkButton.Content = "Ok";
+                break;
+
+            case DialogType.Confirm:
+                QuestionIcon.IsVisible = true;
+                AbortButton.IsVisible = true;
+                OkButton.Content = "Ok";
+                break;
+        }
+    }
+
+    private void OkButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        this.Close(true);
+    }
+
+    private void AbortButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        this.Close(false);
+    }
+}
