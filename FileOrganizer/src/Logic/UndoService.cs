5764f58ba4bbe5b21e369f1a47383ff40135173c
Tzavi727 <tzaviknows@gmail.com>
2026-05-29 18:21:17 -0300

    feat: implement UndoService and session recording logic

---
+++ FileOrganizer/src/Logic/UndoService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileOrganizer.src.Logic
+{
+    internal class UndoService
+    {
+        private static List<(string Name, string OldPath, string NewPath)> currentAction = new();
+
+        private static Stack<List<(string Name, string OldPath, string NewPath)>> undoStack = new();
+
+        public static void AddToCurrentAction(string fileName, string oldPath, string newPath)
+        {
+            currentAction.Add((fileName, oldPath, newPath));
+        }
+
+        public static void CommitLastAction()
+        {
+            if (currentAction.Count > 0)
+            {
+                undoStack.Push(new List<(string Name, string OldPath, string NewPath)>(currentAction));
+                ClearCurrentActionList();
+            }
+        }
+        public static void ClearCurrentActionList()
+        {
+            currentAction.Clear();
+        }
+    }
+}
