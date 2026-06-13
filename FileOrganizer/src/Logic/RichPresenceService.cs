e1a34cd6bfc1efca8def4ddeee620a4d50d43858
Tzavi727 <tzaviknows@gmail.com>
2026-06-13 16:54:19 -0300

    feat: implement RichPresenceService logic for Discord integration

---
+++ FileOrganizer/src/Logic/RichPresenceService.cs
@@ -0,0 +1,74 @@
+﻿using Avalonia.Metadata;
+using DiscordRPC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FileOrganizer.src.Logic
+{
+    internal class RichPresenceService
+    {
+        public static DiscordRpcClient? Client { get; private set; }
+        private static CancellationTokenSource? _timerCts;
+
+        public static void InitializeRpc()
+        {
+            Client = new DiscordRpcClient("1515420956527693944");
+            Client.Initialize();
+        }
+
+        public static void DisposeClient()
+        {
+            Client?.Dispose();
+        }
+
+        public static void SetIdlePresence()
+        {
+            Client?.SetPresence(new RichPresence()
+            {
+                Details = "Idle",
+                State = "Waiting for Action...",
+                Assets = new Assets()
+                {
+                    LargeImageKey = "wolf.png",
+                    LargeImageText = "File Organizer by Tzavi"
+                }
+            });
+        }
+
+        public static async void SetActionPresence(string details, string state)
+        {
+            _timerCts?.Cancel();
+            _timerCts = new CancellationTokenSource();
+            var token = _timerCts.Token;
+
+            try
+            {
+                UpdateStatus(details, state);
+                await Task.Delay(120000, token);
+                SetIdlePresence();
+            }
+            catch (TaskCanceledException)
+            {
+
+            }
+        }
+
+        public static void UpdateStatus(string details, string state)
+        {
+            Client?.SetPresence(new RichPresence()
+            {
+                Details = details,
+                State = state,
+                Assets = new Assets()
+                {
+                    LargeImageKey = "wolf.png",
+                    LargeImageText = "File Organizer by Tzavi"
+                }
+            });
+        }
+    }
+}
